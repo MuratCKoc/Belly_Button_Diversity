a81a2b488e49f7f2ed0b7fa27d12a7cf865a8e48
MuratCKOC <muratcagrikoc@gmail.com>
2021-02-02 09:43:33 -0500

    populate selector

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,40 @@
-// Sort the data by OTU
-function retreiveData(sample) {
-d3.json("samples.json").then((data)=> {
-    console.log(data)
-});
+// d3 bindings to html
+var options = d3.select("#select")
+var selector = d3.select("#selDataset");
+var demographics = d3.select("#demographics");
+
+
+// Main function
+function chart_it() {
+    populate_options();
+    //var otu_id = options.node().value
+}
+
+function init() {
+    d3.json("samples.json").then((data) =>  {
+        var Names = data.names;
+        Names.forEach((sample) => {
+            selector.append("option").text(sample).property("value",sample);
+        });
+        console.log(Names);
+    });
 };
-retreiveData();
+
+// Populate the OTU options
+//function populate_options() {
+//}
+
+// Sort the data by OTU
+
+
+// Init function
+//function populate_(otu_ids) {
+//d3.json("samples.json").then((data)=> {
+//    var sampleNameArray = data.names;
+//
+//    console.log(data)
+//});
+//}
+
+// Event listener
+options.on("change", init());
